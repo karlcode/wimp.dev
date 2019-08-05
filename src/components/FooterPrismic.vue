0fa63c8ab2fab3918840045015bf67bc5e15e9c5
karlcode <karl.l1@hotmail.com>
2019-08-06 09:24:15 +1000

    Minor changes

--- src/components/FooterPrismic.vue
+++ src/components/FooterPrismic.vue
@@ -1,8 +1,6 @@
 <template>
   <footer class="footer">
-    <a href="https://prismic.io" target="_blank" rel="noopener">
-      <img class="logo" src="../assets/img/prismic-logo.svg" alt="Prismic" />
-    </a>
+    <p>A blog for everyone</p>
   </footer>
 </template>
 
